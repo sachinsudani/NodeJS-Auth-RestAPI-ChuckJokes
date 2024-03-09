d506400078aaccea7ce06bf1db8cd7b8ac360378
Sachin Sudani <sachinsudani60@gmail.com>
2024-03-09 20:14:15 +0530

    feat: create node server

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,25 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-// import userRoutes from './routes/userRoutes';
+import userRoutes from './routes/userRoutes';
 // import chuckNorrisRoutes from './routes/chuckNorrisRoutes';
 import mongoose from 'mongoose';
+import { errorHandler } from './middleware/errorHandlerMiddleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(cors());
+app.use(express.json());
 
-// Database Connection
-const MONGODB_URI = 'mongodb://localhost:27017/user_management';
-mongoose.connect(MONGODB_URI, {
-	// useNewUrlParser: true,
-	// useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://localhost:27017/user_management');
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-	console.log('Connected to MongoDB');
-});
 
-// Routes
-// app.use('/api/users', userRoutes);
+db.on('error', console.error.bind(console, 'connection error:'));
+
+app.use('/api/users', userRoutes);
 // app.use('/api', chuckNorrisRoutes);
 
-// Global Error Handler
-app.use(
-	(
-		err: Error,
-		req: express.Request,
-		res: express.Response,
-		next: express.NextFunction
-	) => {
-		console.error(err.stack);
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
-);
+app.use(errorHandler);
 
-// Start Server
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
